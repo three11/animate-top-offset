0f2cd1c50a54107304746962d141b0929315735a
Atanas Atanasov <scriptex.bg@gmail.com>
2018-09-18 08:20:44 +0300

    Update dist

--- dist/animate-top-offset.js
+++ dist/animate-top-offset.js
@@ -1,161 +1,70 @@
-(function webpackUniversalModuleDefinition(root, factory) {
-	if(typeof exports === 'object' && typeof module === 'object')
-		module.exports = factory();
-	else if(typeof define === 'function' && define.amd)
-		define([], factory);
-	else if(typeof exports === 'object')
-		exports["animateTopOffset"] = factory();
-	else
-		root["animateTopOffset"] = factory();
-})(window, function() {
-return /******/ (function(modules) { // webpackBootstrap
-/******/ 	// The module cache
-/******/ 	var installedModules = {};
-/******/
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/
-/******/ 		// Check if module is in cache
-/******/ 		if(installedModules[moduleId]) {
-/******/ 			return installedModules[moduleId].exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = installedModules[moduleId] = {
-/******/ 			i: moduleId,
-/******/ 			l: false,
-/******/ 			exports: {}
-/******/ 		};
-/******/
-/******/ 		// Execute the module function
-/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-/******/
-/******/ 		// Flag the module as loaded
-/******/ 		module.l = true;
-/******/
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/
-/******/
-/******/ 	// expose the modules object (__webpack_modules__)
-/******/ 	__webpack_require__.m = modules;
-/******/
-/******/ 	// expose the module cache
-/******/ 	__webpack_require__.c = installedModules;
-/******/
-/******/ 	// define getter function for harmony exports
-/******/ 	__webpack_require__.d = function(exports, name, getter) {
-/******/ 		if(!__webpack_require__.o(exports, name)) {
-/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
-/******/ 		}
-/******/ 	};
-/******/
-/******/ 	// define __esModule on exports
-/******/ 	__webpack_require__.r = function(exports) {
-/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 		}
-/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 	};
-/******/
-/******/ 	// create a fake namespace object
-/******/ 	// mode & 1: value is a module id, require it
-/******/ 	// mode & 2: merge all properties of value into the ns
-/******/ 	// mode & 4: return value when already ns object
-/******/ 	// mode & 8|1: behave like require
-/******/ 	__webpack_require__.t = function(value, mode) {
-/******/ 		if(mode & 1) value = __webpack_require__(value);
-/******/ 		if(mode & 8) return value;
-/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
-/******/ 		var ns = Object.create(null);
-/******/ 		__webpack_require__.r(ns);
-/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
-/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
-/******/ 		return ns;
-/******/ 	};
-/******/
-/******/ 	// getDefaultExport function for compatibility with non-harmony modules
-/******/ 	__webpack_require__.n = function(module) {
-/******/ 		var getter = module && module.__esModule ?
-/******/ 			function getDefault() { return module['default']; } :
-/******/ 			function getModuleExports() { return module; };
-/******/ 		__webpack_require__.d(getter, 'a', getter);
-/******/ 		return getter;
-/******/ 	};
-/******/
-/******/ 	// Object.prototype.hasOwnProperty.call
-/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
-/******/
-/******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "";
-/******/
-/******/
-/******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 0);
-/******/ })
-/************************************************************************/
-/******/ ([
-/* 0 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = void 0;
-var win = window;
-var easingEquations = {
-  easeOutSine: function easeOutSine(pos) {
-    return Math.sin(pos * (Math.PI / 2));
-  },
-  easeInOutSine: function easeInOutSine(pos) {
-    return -0.5 * (Math.cos(Math.PI * pos) - 1);
-  },
-  easeInOutQuint: function easeInOutQuint(pos) {
-    if ((pos /= 0.5) < 1) {
-      return 0.5 * Math.pow(pos, 5);
-    }
-
-    return 0.5 * (Math.pow(pos - 2, 5) + 2);
+(function (global, factory) {
+  if (typeof define === "function" && define.amd) {
+    define(["exports"], factory);
+  } else if (typeof exports !== "undefined") {
+    factory(exports);
+  } else {
+    var mod = {
+      exports: {}
+    };
+    factory(mod.exports);
+    global.animateTopOffset = mod.exports;
   }
-};
+})(this, function (_exports) {
+  "use strict";
 
-var requestAnimFrame = function () {
-  return win.requestAnimationFrame || win.webkitRequestAnimationFrame || win.mozRequestAnimationFrame || function (callback) {
-    win.setTimeout(callback, 1000 / 60);
-  };
-}();
+  Object.defineProperty(_exports, "__esModule", {
+    value: true
+  });
+  _exports.default = void 0;
+  var win = window;
+  var easingEquations = {
+    easeOutSine: function easeOutSine(pos) {
+      return Math.sin(pos * (Math.PI / 2));
+    },
+    easeInOutSine: function easeInOutSine(pos) {
+      return -0.5 * (Math.cos(Math.PI * pos) - 1);
+    },
+    easeInOutQuint: function easeInOutQuint(pos) {
+      if ((pos /= 0.5) < 1) {
+        return 0.5 * Math.pow(pos, 5);
+      }
 
-var animateTopOffset = function animateTopOffset() {
-  var offset = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 0;
-  var container = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : win;
-  var speed = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 2000;
-  var easing = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : 'easeOutSine';
-  var currentTime = 0;
-  var scrollY = container.scrollY || document.documentElement.scrollTop;
-  var time = Math.max(0.1, Math.min(Math.abs(scrollY - offset) / speed, 0.8));
-
-  var tick = function tick() {
-    currentTime += 1 / 60;
-    var p = currentTime / time;
-    var t = easingEquations[easing](p);
-    var pos = p < 1 ? scrollY + (offset - scrollY) * t : offset;
-
-    if (p < 1) {
-      requestAnimFrame(tick);
+      return 0.5 * (Math.pow(pos - 2, 5) + 2);
     }
-
-    container.scrollTo(0, pos);
   };
 
-  tick();
-};
+  var requestAnimFrame = function () {
+    return win.requestAnimationFrame || win.webkitRequestAnimationFrame || win.mozRequestAnimationFrame || function (callback) {
+      win.setTimeout(callback, 1000 / 60);
+    };
+  }();
 
-var _default = animateTopOffset;
-exports.default = _default;
+  var animateTopOffset = function animateTopOffset() {
+    var offset = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 0;
+    var container = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : win;
+    var speed = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 2000;
+    var easing = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : 'easeOutSine';
+    var currentTime = 0;
+    var scrollY = container.scrollY || document.documentElement.scrollTop;
+    var time = Math.max(0.1, Math.min(Math.abs(scrollY - offset) / speed, 0.8));
 
-/***/ })
-/******/ ])["default"];
+    var tick = function tick() {
+      currentTime += 1 / 60;
+      var p = currentTime / time;
+      var t = easingEquations[easing](p);
+      var pos = p < 1 ? scrollY + (offset - scrollY) * t : offset;
+
+      if (p < 1) {
+        requestAnimFrame(tick);
+      }
+
+      container.scrollTo(0, pos);
+    };
+
+    tick();
+  };
+
+  var _default = animateTopOffset;
+  _exports.default = _default;
 });
